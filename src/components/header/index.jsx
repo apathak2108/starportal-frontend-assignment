e35355d9c76378e52ed1ad9cd75ca361eedca4f7
Anany Pathak <propanany@gmail.com>
2024-07-06 19:01:02 +0530

    header initial ui

---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledHeader } from "./header.styled";
+
+const Header = () => {
+    return (
+        <StyledHeader>
+
+        </StyledHeader>
+    );
+}
+
+export default Header;
